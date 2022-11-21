da2c31f4758192ab24705f82f42594ab3992fb8b
Tomas <tomibudeguer20@gmail.com>
2022-11-21 18:35:47 -0300

    agregue el link de la bd global y agregue vercel

--- src/database.js
+++ src/database.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
-const url = 'mongodb://127.0.0.1:27017/proyecto-final'
+// const url = 'mongodb://127.0.0.1:27017/proyecto-final' //BD-LOCAL
+const url = 'mongodb+srv://TomasBudeguer:KJI5kQLnLz619pLl@cluster0.vjacqdu.mongodb.net/proyecto-final' //BD-GLOBAL
 
 mongoose.connect(url)
 
